497984a58eef505eb6d7d07400b697b72669b937
박 민재 <sobu0715@gmail.com>
2019-10-13 05:16:43 +0000

    feat: Application Setting

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # 2019-OSAM-Backend
 2019년 국방부 오픈소스 아카데미에서 사용할 서버를 준비 하기 위한 Repository입니다.
+
+## Framework
+- **django** : Python 기반 웹 서버 프레임워크
+
+<p align="center">
+    <img width="400px" src="https://www.djangoproject.com/m/img/logos/django-logo-positive.png">
+</p>
+
+## Library
+- **djangorestframework** : Django로 REST API서버를 만드는 데 도움을 준다.
+- **djangorestframework-jwt** : JWT 기반 Authentication을 구현 하는데 사용.
+
+## License
+- MIT
---
+++ user/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    start_date = models.DateField()  # 입대 일자
+    end_date = models.DateField()  # 제대 일자
+    private_first_class = models.DateField()  # 일병
+    corparal = models.DateField()  # 상병
+    sergeant = models.DateField()  # 병장
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    say = models.CharField(max_length=128)  # 좌우명
+    regular_holiday = models.IntegerField(default=26)  # 정기 휴가
+    reward_holiday = models.IntegerField(default=0)  # 포상 휴가
+    consolation_holiday = models.IntegerField(default=0)  # 위로 휴가
+    used_holiday = models.IntegerField(default=0)  # 사용한 휴가
+    checked_holiday = models.IntegerField(default=0)  # 사용 예정 휴가
+    
+    def __str__(self):
+        return "{}{}".format(self.user.first_name, self.user.last_name)
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        
+
+    
