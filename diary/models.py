2fc796b5b9c15e762c7b1a664c6757b068493825
박 민재 <sobu0715@gmail.com>
2019-10-13 05:23:13 +0000

    feat: Diary Model Definition

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Diary:
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    written = models.DateField()
+    title = models.CharField(max_length=128)
+    summary = models.CharField(max_length=256)
+    content = models.TextField()
+    
+    def __str__(self):
+        return "{}{} - {}".format(self.user.first_name, self.user.last_name, self.title)
+    
+    def save(self, *args, **kwargs):
+        self.summary = self.content[:256]
+        super().save(*args, **kwargs)
