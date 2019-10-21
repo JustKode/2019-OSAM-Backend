a6b3b329009d060bb9939d4a588e4978e794ef01
JustKode <sobu0715@gmail.com>
2019-10-21 23:44:24 +0900

    fix: Schedule Errors

--- schedule/urls.py
+++ schedule/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from schedule import views
 
 urlpatterns = [
+    path('list/', views.get_schedule_list),
     path('list/<int:year>/<int:month>/', views.get_schedule_list),
     path('write/', views.post_schedule),
     path('<int:pk>/', views.schedule)
--- schedule/views.py
+++ schedule/views.py
@@ -12,25 +12,32 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 
 @api_view(['GET'])
-@authentication_classes((IsAuthenticated,))
-def get_schedule_list(request, year=date.today().day, month=date.today().month):
+@permission_classes((IsAuthenticated,))
+def get_schedule_list(request, year=date.today().year, month=date.today().month):
     user = request.user
-    schedule_list = Schedule.objects.filter(user=user).order_by('start_date')
-    result = ScheduleSimpleSerializer(schedule_list)
+    schedule_list = Schedule.objects.filter(
+        user=user,
+        start_date__year__gte=year,
+        start_date__month__gte=month,
+        start_date__year__lte=year,
+        start_date__month__lte=month
+    ).order_by('start_date')
+    result = ScheduleSimpleSerializer(schedule_list, many=True)
     
     return Response(result.data, status=status.HTTP_200_OK)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-@authentication_classes((IsAuthenticated,))
+@permission_classes((IsAuthenticated,))
 def schedule(request, pk):
     user = request.user
-    schedule = Schedule.objects.get(id=pk)
-    if schedule.user != user:
+    data = request.data
+    schedule = Schedule.objects.filter(id=pk)
+    if schedule[0].user != user:
         return Response({"message": "user does not match"}, status=status.HTTP_403_FORBIDDEN)
     
     if request.method == 'GET':
-        result = ScheduleSerializer(schedule)
+        result = ScheduleSerializer(schedule[0])
         return Response(result.data, status=status.HTTP_200_OK)
     elif request.method == 'PUT':
         fields = ('schedule_type', 'title', 'content', 'start_date', 'end_date')
@@ -38,8 +45,8 @@ def schedule(request, pk):
         if any(i not in fields for i in data):
             return Response({"message": "invalid fields"}, status=status.HTTP_400_BAD_REQUEST)
         else:
-            schedule = schedule.update(**request.data)
-            result = ScheduleSerializer(schedule)
+            schedule.update(**data)
+            result = ScheduleSerializer(schedule[0])
             return Response(result.data, status=status.HTTP_202_ACCEPTED)
     else:
         schedule.delete()
@@ -47,9 +54,10 @@ def schedule(request, pk):
     
 
 @api_view(['POST'])
-@authentication_classes((IsAuthenticated,))
+@permission_classes((IsAuthenticated,))
 def post_schedule(request):
     user = request.user
+    data = request.data
     if all(i in data for i in ('schedule_type', 'title', 'content', 'start_date', 'end_date')):
         data['user'] = user
         schedule = Schedule.objects.create(**data)
