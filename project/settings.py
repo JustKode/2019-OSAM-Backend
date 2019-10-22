b333bd39029b6562080e138622cc41e1754af213
JustKode <sobu0715@gmail.com>
2019-10-22 20:22:29 +0900

    fix: permission classes

--- project/settings.py
+++ project/settings.py
@@ -107,9 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
-   'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
