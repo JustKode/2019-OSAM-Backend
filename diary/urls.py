e10d4c17c3d9e01136465755d59c1bc786292d8f
JustKode <sobu0715@gmail.com>
2019-10-24 20:02:42 +0900

    feat: URL

--- diary/urls.py
+++ diary/urls.py
@@ -4,6 +4,7 @@ from diary import views
 urlpatterns = [
     path('list/', views.get_diary_list),
     path('list/<int:year>/<int:month>/', views.get_diary_list),
+    path('day/<int:year>/<int:month>/<int:day>', views.getDiaryByDate),
     path('write/', views.post_diary),
     path('<int:pk>/', views.diary)
 ]
